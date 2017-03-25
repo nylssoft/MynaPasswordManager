0573af6ceb93a492e3b2db4c84da4fa0e03acadf
nylssoft <nyls@aol.com>
2017-03-25 09:12:30 +0100

    added welcome and banner PNG for installion
    - set version to 1.0.3.0

--- PasswordGenerator/Properties/AssemblyInfo.cs
+++ PasswordGenerator/Properties/AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
 [assembly: NeutralResourcesLanguage("")]
 [assembly: Guid("08541979-2028-4804-B068-8B6EF978A235")]
 
